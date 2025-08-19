957e682baf3035124b2a3ba6ae90d80d433ab773
Dominik Baumann <dominik.baumann@bbv.ch>
2025-08-19 14:06:49 +0200

    Add a basic solution for v3

---
+++ src/LambdaTale.v3.Tests/ScenarioTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace LambdaTale.v3.Tests;
+
+public class ScenarioTests
+{
+    [Fact]
+    public void ShouldFail() => Assert.True(false);
+}
---
+++ src/LambdaTale.v3/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace LambdaTale.v3;
+
+public class Class1
+{
+}
