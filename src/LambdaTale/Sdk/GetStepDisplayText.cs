3af5a501e31b38483fce0d7ec24936252f47122b
Dominik Baumann <dominik.baumann@bbv.ch>
2025-08-19 13:54:49 +0200

    Clean slate

--- src/LambdaTale/Sdk/GetStepDisplayText.cs
+++
@@ -1,9 +0,0 @@
-namespace LambdaTale.Sdk;
-
-/// <summary>
-/// Gets the display text for a step.
-/// </summary>
-/// <param name="stepText">The step text.</param>
-/// <param name="isBackgroundStep">A value indicating whether the step is a background step.</param>
-/// <returns>A string representing the display text for the step.</returns>
-public delegate string GetStepDisplayText(string stepText, bool isBackgroundStep);
